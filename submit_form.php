2d83e2421f213f223fa86bbf2b739d2b2b49adc4
Jesper Heikkilä <jesper@mystiskdesign.com>
2016-04-13 22:47:30 +0200

    lovely alerts
    lovely alerts

--- submit_form.php
+++ submit_form.php
@@ -3,8 +3,8 @@
     if ($_SERVER['REQUEST_METHOD'] == "POST") {
 
         if (!$_POST['firstname'] || !$_POST['lastname'] || !$_POST['position']) {
-            echo "<p>Please supply all of the data!</p>";
-            exit;
+            echo '<div class="alert alert-danger alert-dismissible" role="alert"><button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>Please supply all of the data</div>';
+            //exit;
         } else {
             try {        
                 require('db.php');
@@ -19,15 +19,18 @@
             } catch (PDOException $e) {
                 echo $e->getMessage();
             }
-            echo "<p>Data submitted successfully</p>";
+            echo '<div class="alert alert-success alert-dismissible" role="alert"><button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>Data submitted successfully</div>';
         }
     }
-
-    echo '<form class="col-md-4" method="POST" action="">';
-    echo '<label for="firstname">Firstname:</label> <input type="text" class="form-control" name="firstname"><br />';
-    echo '<label for="firstname">Lastname:</label> <input type="text" class="form-control" name="lastname"><br />';
-    echo '<label for="firstname">Position:</label> <input type="text" class="form-control" name="position"><br />';
-	echo '<input type="submit" value="Submit"></form>';
+    echo '<div class="collapse navbar-collapse">';
+    echo '<form class="col-md-4 navbar-form" method="POST" action="'.$_SERVER['PHP_SELF'].'">';
+    echo '<div class="form-group">';
+    echo '<label class="sr-only" for="firstname">Firstname:</label> <input type="text" class="form-control" name="firstname" placeholder="Firstname"><br />';
+    echo '<label class="sr-only" for="lastname">Lastname:</label> <input type="text" class="form-control" name="lastname" placeholder="Lastname"><br />';
+    echo '<label class="sr-only" for="position">Position:</label> <input type="text" class="form-control" name="position" placeholder="Position"><br />';
+	echo '<input type="submit" class="btn btn-default" value="Submit"></form>';
+    echo '</div>';
+    echo '</div>';
 
 
 

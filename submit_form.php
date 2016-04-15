0514d19fb6b21e561af85cec18c51b8461f5e261
Jesper Heikkilä <jesper@mystiskdesign.com>
2016-04-15 08:59:04 +0200

    Page reload, message cookie
    Made the page reload on submit form and store message in cookie.

--- submit_form.php
+++ submit_form.php
@@ -16,20 +16,30 @@
 
                 $STH->execute();
 
+                $add_member = "show_message";
+                setcookie('added_member',$add_member);
+                echo "<meta http-equiv='refresh' content='0'>";
             } catch (PDOException $e) {
                 echo $e->getMessage();
             }
-            echo '<div class="alert alert-success alert-dismissible" role="alert"><button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>Data submitted successfully</div>';
+
+
+
         }
     }
+        if(isset($_COOKIE["added_member"])) {
+    echo '<div class="alert alert-success alert-dismissible" role="alert"><button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>Data submitted successfully</div>';
+        setcookie('added_member', '', time()-60*60*24*365);
+    }
+
     echo '<div class="collapse navbar-collapse">';
-    echo '<form class="col-md-4 navbar-form" method="POST" action="'.$_SERVER['PHP_SELF'].'">';
-    echo '<div class="form-group">';
-    echo '<label class="sr-only" for="firstname">Firstname:</label> <input type="text" class="form-control" name="firstname" placeholder="Firstname"><br />';
-    echo '<label class="sr-only" for="lastname">Lastname:</label> <input type="text" class="form-control" name="lastname" placeholder="Lastname"><br />';
-    echo '<label class="sr-only" for="position">Position:</label> <input type="text" class="form-control" name="position" placeholder="Position"><br />';
-	echo '<input type="submit" class="btn btn-default" value="Submit"></form>';
-    echo '</div>';
+        echo '<form class="col-md-4 navbar-form" method="POST" action="">';
+            echo '<div class="form-group">';
+            echo '<label class="sr-only" for="firstname">Firstname:</label> <input type="text" class="form-control" name="firstname" placeholder="Firstname"><br />';
+            echo '<label class="sr-only" for="lastname">Lastname:</label> <input type="text" class="form-control" name="lastname" placeholder="Lastname"><br />';
+            echo '<label class="sr-only" for="position">Position:</label> <input type="text" class="form-control" name="position" placeholder="Position"><br />';
+        	echo '<input type="submit" class="btn btn-default" value="Submit"></form>';
+        echo '</div>';
     echo '</div>';
 
 

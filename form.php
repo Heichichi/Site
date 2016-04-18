5276c0468d9596c89276eeb5f760e2f8fb92ef23
Jesper Heikkilä <jesper@mystiskdesign.com>
2016-04-18 22:35:29 +0200

    File upload work, name to db in progress
    File upload work, name to db in progress

---
+++ form.php
@@ -0,0 +1,12 @@
+<div class="collapse navbar-collapse">
+	<form class="col-md-4 navbar-form" method="POST" action="" enctype="multipart/form-data">
+		<div class="form-group">
+			<label class="sr-only" for="firstname">Firstname:</label> <input type="text" class="form-control" name="firstname" placeholder="Firstname"><br />
+			<label class="sr-only" for="lastname">Lastname:</label> <input type="text" class="form-control" name="lastname" placeholder="Lastname"><br />
+			<label class="sr-only" for="position">Position:</label> <input type="text" class="form-control" name="position" placeholder="Position"><br />
+			<label class="" for="fileToUpload">Select image to upload:</label>
+    		<input type="file" class="btn btn-default" name="fileToUpload" id="fileToUpload">
+			<input type="submit" class="btn btn-default" value="Submit">
+		</div>
+	</form>
+</div>
--- submit_form.php
+++ submit_form.php
@@ -1,11 +1,14 @@
 <?php 
-	    # basic pdo connection (with added option for error handling)
+	# basic pdo connection (with added option for error handling)
     if ($_SERVER['REQUEST_METHOD'] == "POST") {
+
+
 
         if (!$_POST['firstname'] || !$_POST['lastname'] || !$_POST['position']) {
             echo '<div class="alert alert-danger alert-dismissible" role="alert"><button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>Please supply all of the data</div>';
             //exit;
-        } else {
+        }
+         else {
             try {        
                 require('db.php');
                 $STH = $conn->prepare("INSERT INTO user (firstname,lastname,position) VALUES (:firstname,:lastname,:position)");
@@ -14,11 +17,21 @@
                 $STH->bindParam(':lastname', $_POST['lastname']);
                 $STH->bindParam(':position', $_POST['position']);
 
-                $STH->execute();
+                // Include file upload functionality
+                require("upload.php");
 
-                $add_member = "show_message";
-                setcookie('added_member',$add_member);
-                echo "<meta http-equiv='refresh' content='0'>";
+                $STH->bindParam(':fileToUpload', $target_file);
+                
+
+                if($error_message_upload == "") {
+                    $STH->execute();
+                    $add_member = "show_message";
+                    setcookie('added_member',$add_member);
+                    echo "<meta http-equiv='refresh' content='0'>";
+                }
+                else {
+                    echo $error_message_upload;
+                }
             } catch (PDOException $e) {
                 echo $e->getMessage();
             }
@@ -28,21 +41,12 @@
         }
     }
         if(isset($_COOKIE["added_member"])) {
-    echo '<div class="alert alert-success alert-dismissible" role="alert"><button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>Data submitted successfully</div>';
+            echo '<div class="alert alert-success alert-dismissible" role="alert"><button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>Data submitted successfully</div>';
+        //Clear the cookie
         setcookie('added_member', '', time()-60*60*24*365);
     }
 
-    echo '<div class="collapse navbar-collapse">';
-        echo '<form class="col-md-4 navbar-form" method="POST" action="">';
-            echo '<div class="form-group">';
-            echo '<label class="sr-only" for="firstname">Firstname:</label> <input type="text" class="form-control" name="firstname" placeholder="Firstname"><br />';
-            echo '<label class="sr-only" for="lastname">Lastname:</label> <input type="text" class="form-control" name="lastname" placeholder="Lastname"><br />';
-            echo '<label class="sr-only" for="position">Position:</label> <input type="text" class="form-control" name="position" placeholder="Position"><br />';
-        	echo '<input type="submit" class="btn btn-default" value="Submit"></form>';
-        echo '</div>';
-    echo '</div>';
-
-
+    require("form.php");
 
     # close the connection  
     $DBH = null;
